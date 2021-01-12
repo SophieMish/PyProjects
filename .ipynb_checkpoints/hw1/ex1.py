3c454086fe55d43c8d36488a912a22de90199607
SophieMish <58442310+SophieMish@users.noreply.github.com>
2021-01-12 19:46:45 +0300

    my local changes

--- .ipynb_checkpoints/hw1/ex1.py
+++
@@ -1,7 +0,0 @@
-import re
-
-num = int(input())
-pingv = """   _~_\n  (o o)   \n /  V  \ \n/(  _  )\ \n  ^^ ^^)"""
-for i in range(1, num + 1):
-    print(pingv,sep='')
-
--- gameLect/CYGNO.py
+++ gameLect/CYGNO.py
@@ -1,14 +1,57 @@
+import random
+
+
 class CYGNO:
 
     def __init__(self):
         pass
 
     def guys(self):
-        return 'IAD makes me sad('
+        return 'Kosterin,Mishukova,Papko,Yuzhakov'
+
+    def utility(self):
+        val = 0
+        for k in self.enemy_suggestion.keys():
+            val += self.my_value[k] * self.enemy_suggestion[k]
+        return val
 
     def turn(self):
-        val = 0
-        for k in enemy_suggestion.keys():
-            val += my_value[k] * enemy_suggestion[k]
-        if val != 0:
-            self.suggestion = 'OK'
+        if self.round == 0:
+            if self.me_first:
+                self.suggestion = {
+                    "Book": 1,
+                    "Hat": 2,
+                    "Ball": 3,
+                }
+            else:
+                max_util = self.my_value['Book'] * 1 + self.my_value['Hat'] * 2 + self.my_value['Ball'] * 3
+                if self.utility() < max_util / 2:
+                    self.suggestion = {
+                            "Book": random.randint(0,1),
+                            "Hat": 1,
+                            "Ball": 3,
+                    }
+                else:
+                    self.suggestion = 'OK'
+
+        else:
+            if self.me_first:
+                self.suggestion = {
+                    "Book": 1,
+                    "Hat": 2,
+                    "Ball": 3,
+                }
+            else:
+                if self.utility() > 0:
+                    self.suggestion = {
+                        "Book": random.randint(0, 1),
+                        "Hat": 1,
+                        "Ball": 3,
+                    }
+                else:
+                    self.suggestion = 'OK'
+
+
+Haggler = CYGNO()
+
+
