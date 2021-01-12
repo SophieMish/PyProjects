cdb5d723a8143d69f20dc8a5eb2631a4b58d65e7
SophieMish <58442310+SophieMish@users.noreply.github.com>
2021-01-12 19:43:57 +0300

    projects for data science

---
+++ gameLect/CYGNO.py
@@ -0,0 +1,14 @@
+class CYGNO:
+
+    def __init__(self):
+        pass
+
+    def guys(self):
+        return 'IAD makes me sad('
+
+    def turn(self):
+        val = 0
+        for k in enemy_suggestion.keys():
+            val += my_value[k] * enemy_suggestion[k]
+        if val != 0:
+            self.suggestion = 'OK'
